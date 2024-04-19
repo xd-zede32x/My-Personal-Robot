9b9fecf2691a03ab53f2b580b22668525d1ed129
Zeds_Xd <homenkor24@gmail.com>
2024-04-19 12:11:29 +0300

    Upddate script program

--- My-Personalr-Robot/My-Personalr-Robot/Program.cs
+++ My-Personalr-Robot/My-Personalr-Robot/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace My_Personalr_Robot
+namespace My_Personal_Robot
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main(string[] _)
         {
-
+            UserGreeting();
         }
+
+        private static void UserGreeting()
+        {
+            Console.Title = "Альбедо";
+            ChangeConsoleColor(ConsoleColor.Yellow);
+
+            Console.WriteLine("Приветствую вас мой господин это говорит ваша Альбедо.");
+            Console.Write("\nКак я могу к вам обращаться мой повелитель (--╳--): ");
+
+            EnteringUserName();
+        }
+
+        private static void EnteringUserName()
+        {
+            string userName = Console.ReadLine();
+            Console.WriteLine($"\nХорошо я буду вас называть {userName} если вы не против (-- --)");
+        }
+
+        private static void ChangeConsoleColor(ConsoleColor color) => Console.ForegroundColor = color;
     }
 }
